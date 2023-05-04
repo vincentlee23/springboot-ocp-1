ff2888e6e0622cd06a82ccb74c0d45df1fff9e92
Vincent <102410191+vincentlee23@users.noreply.github.com>
2023-05-04 14:26:42 +0700

    Add files via upload

---
+++ springboot-ocp-1/src/main/java/com/example/springbootocp1/SpringbootOcp1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootocp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOcp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOcp1Application.class, args);
+    }
+
+}
